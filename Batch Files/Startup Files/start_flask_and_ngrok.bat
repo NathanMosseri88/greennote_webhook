69a0c8a65e265fbe20774460c3dddfd464ce93ac
Nathan Mosseri <nathan@simpletowork.com>
2025-01-09 14:30:07 -0500

    configuration

--- Batch Files/Startup Files/start_flask_and_ngrok.bat
+++ Batch Files/Startup Files/start_flask_and_ngrok.bat
@@ -3,7 +3,7 @@ cd /d "C:\Users\kyf_achworks\Desktop\New Projects\Greennote\Greennote_webhook>"
 
 call venv\Scripts\activate
 
-start "Flask App" cmd /k "python app.py"
+start "Flask App" cmd /k "python main.py"
 
 start "ngrok Tunnel" cmd /k "C:\Users\kyf_achworks\Desktop\ngrok.exe http --domain=one-imp-mistakenly.ngrok-free.app 8080"
 
--- Batch Files/configure.bat
+++ Batch Files/configure.bat
@@ -1,14 +1,14 @@
 @echo off
 
-pip install virtualenv
+C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python312\scripts\pip3 install virtualenv
 
 cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Greennote\Greennote_webhook"
-python -m virtualenv venv
+C:\Users\kyf_achworks\AppData\Local\Programs\Python\Python312\python -m virtualenv venv
 
 cd venv/scripts
 call activate.bat
 
 cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Greennote\Greennote_webhook
-pip install -r requirements.txt
+C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python312\scripts\pip3 install -r requirements.txt
 
 cmd /k
