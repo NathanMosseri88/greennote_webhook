a407c5df42e364c020c584331e00877791a035e6
Nathan Mosseri <nathan@simpletowork.com>
2025-02-27 10:32:34 -0500

    ngrok logging

--- Batch Files/start_flask_and_ngrok.bat
+++ Batch Files/start_flask_and_ngrok.bat
@@ -5,6 +5,6 @@ call venv\Scripts\activate
 
 start "Flask App" cmd /k "python main.py"
 
-start "ngrok Tunnel" cmd /k "C:\Users\kyf_achworks\Desktop\ngrok.exe http --domain=one-imp-mistakenly.ngrok-free.app 8080 -log=C:\Users\kyf_achworks\Desktop\ngrok.log"
+start "ngrok Tunnel" cmd /k "C:\Users\kyf_achworks\Desktop\ngrok.exe http --domain=one-imp-mistakenly.ngrok-free.app 8080 --log=C:\Users\kyf_achworks\Desktop\ngrok.log"
 
 
